8cd4676abb6ad1a8b22e00766cd15da9734e19ac
Enzo Ferrari <ferrarienzobf@gmail.com>
2021-01-14 16:16:35 -0300

    Improve JSDocs documentation

--- src/Billing.ts
+++ src/Billing.ts
@@ -1,11 +1,16 @@
-/* eslint-disable import/no-unresolved */
 import * as functions from "firebase-functions";
 
 import { disableBilling } from "./BillingAPI";
 
 import { BillingPubSubResponse, BillingLimiterParams } from "./ts";
 
-export default function createBillingCallback({
+/**
+ * Creates a PubSub functions that listen to a topic
+ * registered in a GCP budget.
+ * @param disableProjectAmount value that when reached, causes
+ * the billing to be disabled.
+ */
+export default function createBillingPubSub({
   disableProjectAmount,
   topicId,
 }: BillingLimiterParams) {
